e6bab23385cf0f44df52319fb7cecc480ec3a2ae
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2021-04-16 00:51:58 +0200

    Linted the whole project

--- src/services/imageSearch/sv_imageSearch.ts
+++ src/services/imageSearch/sv_imageSearch.ts
@@ -3,36 +3,36 @@ import axios from "axios";
 const gis = require("g-i-s");
 
 interface imageData {
-	url: string;
-	width: number;
-	height: number;
+  url: string;
+  width: number;
+  height: number;
 }
 
 export class ImageSearch {
-	constructor() {}
+  constructor() {}
 
-	async getImage(queryParam: string) {
-		return new Promise((resolve, reject) => {
-			gis(queryParam, (error: any, results: any) => {
-				if (error) {
-					reject(error);
-				} else {
-					resolve(results);
-				}
-			});
-		});
-	}
+  async getImage(queryParam: string) {
+    return new Promise((resolve, reject) => {
+      gis(queryParam, (error: any, results: any) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results);
+        }
+      });
+    });
+  }
 
-	async getRandomImage(imageUrl: imageData) {
-		return imageUrl.url;
-	}
+  async getRandomImage(imageUrl: imageData) {
+    return imageUrl.url;
+  }
 
-	async checkImageStatus(imageUrl: string) {
-		try {
-			return await axios.get(imageUrl).then((r) => {
-				return r.status;
-			});
-		} catch (err) {}
-		return null;
-	}
+  async checkImageStatus(imageUrl: string) {
+    try {
+      return await axios.get(imageUrl).then((r) => {
+        return r.status;
+      });
+    } catch (err) {}
+    return null;
+  }
 }
