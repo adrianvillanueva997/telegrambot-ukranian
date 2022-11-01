96d5d084ca51207a2a6c71077d77e9c4f81baf58
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2022-11-01 19:49:35 +0100

    Updated dockerfile and gitignore

--- target/debug/deps/telegrambot_ukranian-2afd3689cdf877f4.d
+++
@@ -1,7 +0,0 @@
-/Users/avm/personal/proyectos/telegrambot-ukranian/target/debug/deps/telegrambot_ukranian-2afd3689cdf877f4.rmeta: src/main.rs
-
-/Users/avm/personal/proyectos/telegrambot-ukranian/target/debug/deps/telegrambot_ukranian-2afd3689cdf877f4.d: src/main.rs
-
-src/main.rs:
-
-# env-dep:CLIPPY_ARGS=
--- target/debug/deps/telegrambot_ukranian-cb4e22efa4d133e4.d
+++
@@ -1,7 +0,0 @@
-/Users/avm/personal/proyectos/telegrambot-ukranian/target/debug/deps/telegrambot_ukranian-cb4e22efa4d133e4.rmeta: src/main.rs
-
-/Users/avm/personal/proyectos/telegrambot-ukranian/target/debug/deps/telegrambot_ukranian-cb4e22efa4d133e4.d: src/main.rs
-
-src/main.rs:
-
-# env-dep:CLIPPY_ARGS=
