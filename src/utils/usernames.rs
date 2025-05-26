adc9420521365469fb7c8445deed03803516f0ff
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2025-05-26 19:47:47 +0900

    remove lazy static and fix clippy issues

--- src/utils/usernames.rs
+++ src/utils/usernames.rs
@@ -1,5 +1,5 @@
-use lazy_static::lazy_static;
 use std::collections::HashMap;
+use std::sync::LazyLock;
 
 #[derive(Debug, Clone, Copy, Hash, Eq, PartialEq)]
 pub enum Username {
@@ -20,27 +20,25 @@ pub enum Username {
     McKay,
 }
 
-lazy_static! {
-    static ref HANDLES: HashMap<Username, &'static str> = {
-        let mut m = HashMap::new();
-        m.insert(Username::TheXiao77, "@adrianv5x");
-        m.insert(Username::Javi, "@LilNarwhal");
-        m.insert(Username::Victor, "@SanZ97xX");
-        m.insert(Username::DarkTrainer, "@DarkTrainer");
-        m.insert(Username::Awe, "@JoseAwe");
-        m.insert(Username::Toxic, "@txc450");
-        m.insert(Username::Red, "@REDMSR");
-        m.insert(Username::Dvdgg, "@Dvdgg");
-        m.insert(Username::Garfu, "@Garfu01");
-        m.insert(Username::Sauturn, "@Sauturn");
-        m.insert(Username::Mario, "@CecilioGil");
-        m.insert(Username::Jaime, "@jaimegsov");
-        m.insert(Username::Davas, "@DavasJoe");
-        m.insert(Username::DrDvd, "@THEDRDVD");
-        m.insert(Username::McKay, "@DoctorMckay");
-        m
-    };
-}
+static HANDLES: LazyLock<HashMap<Username, &'static str>> = LazyLock::new(|| {
+    let mut m = HashMap::new();
+    m.insert(Username::TheXiao77, "@adrianv5x");
+    m.insert(Username::Javi, "@LilNarwhal");
+    m.insert(Username::Victor, "@SanZ97xX");
+    m.insert(Username::DarkTrainer, "@DarkTrainer");
+    m.insert(Username::Awe, "@JoseAwe");
+    m.insert(Username::Toxic, "@txc450");
+    m.insert(Username::Red, "@REDMSR");
+    m.insert(Username::Dvdgg, "@Dvdgg");
+    m.insert(Username::Garfu, "@Garfu01");
+    m.insert(Username::Sauturn, "@Sauturn");
+    m.insert(Username::Mario, "@CecilioGil");
+    m.insert(Username::Jaime, "@jaimegsov");
+    m.insert(Username::Davas, "@DavasJoe");
+    m.insert(Username::DrDvd, "@THEDRDVD");
+    m.insert(Username::McKay, "@DoctorMckay");
+    m
+});
 
 impl Username {
     #[must_use]
