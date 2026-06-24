2b3f210621426598abcca0f56b75993b1fccbc56
Adrián Villanueva <16082421+adrianvillanueva997@users.noreply.github.com>
2026-06-25 00:59:50 +0900

    refactor: Cleanup (#472)

--- src/utils/usernames.rs
+++ src/utils/usernames.rs
@@ -1,6 +1,3 @@
-use std::collections::HashMap;
-use std::sync::LazyLock;
-
 #[derive(Debug, Clone, Copy, Hash, Eq, PartialEq)]
 pub enum Username {
     TheXiao77,
@@ -21,35 +18,26 @@ pub enum Username {
     Andres,
 }
 
-static HANDLES: LazyLock<HashMap<Username, &'static str>> = LazyLock::new(|| {
-    let mut m = HashMap::new();
-    m.insert(Username::TheXiao77, "@adrianv5x");
-    m.insert(Username::Javi, "@LilNarwhal");
-    m.insert(Username::Victor, "@SanZ97xX");
-    m.insert(Username::DarkTrainer, "@DarkTrainer");
-    m.insert(Username::Awe, "@JoseAwe");
-    m.insert(Username::Toxic, "@txc450");
-    m.insert(Username::Red, "@REDMSR");
-    m.insert(Username::Dvdgg, "@Dvdgg");
-    m.insert(Username::Garfu, "@Garfu01");
-    m.insert(Username::Sauturn, "@Sauturn");
-    m.insert(Username::Mario, "@CecilioGil");
-    m.insert(Username::Jaime, "@jaimegsov");
-    m.insert(Username::Davas, "@DavasJoe");
-    m.insert(Username::DrDvd, "@THEDRDVD");
-    m.insert(Username::McKay, "@DoctorMckay");
-    m.insert(Username::Andres, "@AndresMorenoBlu");
-    m
-});
-
 impl Username {
     #[must_use]
     pub fn telegram_handle(&self) -> &'static str {
-        HANDLES[self]
+        match self {
+            Self::TheXiao77 => "@adrianv5x",
+            Self::Javi => "@LilNarwhal",
+            Self::Victor => "@SanZ97xX",
+            Self::DarkTrainer => "@DarkTrainer",
+            Self::Awe => "@JoseAwe",
+            Self::Toxic => "@txc450",
+            Self::Red => "@REDMSR",
+            Self::Dvdgg => "@Dvdgg",
+            Self::Garfu => "@Garfu01",
+            Self::Sauturn => "@Sauturn",
+            Self::Mario => "@CecilioGil",
+            Self::Jaime => "@jaimegsov",
+            Self::Davas => "@DavasJoe",
+            Self::DrDvd => "@THEDRDVD",
+            Self::McKay => "@DoctorMckay",
+            Self::Andres => "@AndresMorenoBlu",
+        }
     }
-}
-
-#[must_use]
-pub fn get_telegram_handle(user: Username) -> &'static str {
-    user.telegram_handle()
 }
